5418bc925a0a176483b29992115149531d54fd4d
Admin <flenivetc@gmail.com>
2021-03-03 20:18:39 +0300

    add filechecker & file Map

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,7 +1,31 @@
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Parser {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            System.out.println("аргумент " + (i + 1) +  " = " + args[i]); //args to stdout check
+
+        Map<File, String> filesMap= new HashMap<>();
+
+        for (String filename : args) {
+
+            File file = new File(filename);
+
+            try {
+                if (file.exists()) filesMap.put(file, getFileExtension(file));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    //метод, извлекающий расширение файла
+    private static String getFileExtension(File file) {
+
+        String fileName = file.getName();
+
+        if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+            return fileName.substring(fileName.lastIndexOf(".")+1);
+        else return "";
     }
 }
