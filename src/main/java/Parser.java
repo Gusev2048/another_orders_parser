23c775cad6b394c507a2b98531caeeec57953097
Admin <flenivetc@gmail.com>
2021-03-03 18:28:14 +0300

    args to stdout check

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,7 +1,7 @@
 public class Parser {
     public static void main(String[] args) {
         for (int i = 1; i < args.length; i++) {
-            System.out.println("аргумент = " + i + args[i]);
+            System.out.println("аргумент = " + i + args[i]); //args to stdout check
         }
     }
 }
